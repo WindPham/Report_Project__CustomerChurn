06737d864e90bd100e4f58137137d0eda2ea623f
WindPham <48479585+WindPham@users.noreply.github.com>
2019-07-26 18:02:27 +0700

    26/7/2019
    Phần tích data chưa fix xong bug
    Phần rời rạc hóa: code xong 3 phương pháp kmean, width, frequency

--- Preprocessing_Data/main/DISCRETISATION.py
+++ Preprocessing_Data/main/DISCRETISATION.py
@@ -2,84 +2,117 @@ import sklearn as skl
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.cluster import KMeans
 from IPython.display import Image
 from IPython.core.display import HTML 
+from sklearn.externals.six import StringIO
 import pydot
 import pydotplus
 import graphviz
-from sklearn.externals.six import StringIO
 import os
 import numpy as np
 
 
 class DISCRETISATION(object):
     """description of class"""
+    #======================================================================================================
     def __init__(self):
-        return;
 
-
-    #===================================================================================================
-
-    def binning_frequency(self, df, col_name):
-        
-        return;
-
-    #===================================================================================================
-
-    def binning_width(self, df, col_name):
 
         return;
-    #===================================================================================================
-
-    def k_mean(self, df, col_name):
-
+    #======================================================================================================
+    def binning_frequency(self, df, col_name, Bins):
+        df.sort_values(col_name);
+        m = int(len(df[col_name])/Bins);
+        for i in range(0,Bins):
+            if(i==Bins-2):
+                df.set_value(range(i*m, len(df[col_name])), col_name, str(i));
+            else:
+                df.set_value(range(i*m, i*m + m), col_name, str(i));
         return;
-
-    #===================================================================================================
+    #======================================================================================================
+    def binning_width(self, df, col_name, Bins):
+        Max = np.max(df[col_name]);
+        Min = np.min(df[col_name]);
+        W = int((Max - Min)/Bins);
+        df[col_name] /= W;
+        df[col_name]=df[col_name].astype(int);
+        df.loc[df[col_name]==Bins, col_name]=Bins-1;
+        df[col_name].astype(str);
+        return;
+    #======================================================================================================
+    def k_mean(self, df, col_name, Bins):
+        model = KMeans(n_clusters = Bins, random_state = 0);
+        model.fit(df[col_name].values.reshape((-1,1)));
+        label = model.labels_;
+        df[col_name] = label;
+        return;
+    #======================================================================================================
     def decision_tree(self, df, trans_col_name, label_col_name, Max_depth = 3, must_not_discre = []):
         if (trans_col_name not in must_not_discre) or len(must_not_discre) == 0:
             tree_model = DecisionTreeClassifier(max_depth = Max_depth);
             tree_model.fit(df[trans_col_name].to_frame(), df[label_col_name].to_frame());
             new_col_name = trans_col_name + "_" + label_col_name + "_Proba";
             predict_proba = tree_model.predict_proba(df[trans_col_name].to_frame());
-            df[trans_col_name] = predict_proba[:,1];
-            df[trans_col_name] = df[trans_col_name].astype(str);
-            return tree_model, trans_col_name;
+            df[new_col_name] = predict_proba[:,1];
+            min_max = pd.concat(
+                [df.groupby([new_col_name])[trans_col_name].min(),
+                 df.groupby([new_col_name])[trans_col_name].max()], 
+                 axis=1);
+            return min_max, trans_col_name;
         return None, trans_col_name;
-    #===================================================================================================
+    #======================================================================================================
     def decision_tree_all_cols(self, df, label_col_name, Max_depth = 3, must_not_discre = []):
         result = [];
         for i in df.columns:
             if df[i].dtypes != 'object' and i != label_col_name:
-                model = self.decision_tree(df, i, label_col_name, Max_depth, must_not_discre);
-                result.append(model);
+                result.append(decision_tree(df, i, label_col_name, Max_depth, must_not_discre));
         return result;
-    #===================================================================================================
+    #======================================================================================================
     def random_forest(self, df, trans_col_name, label_col_name, Max_depth = 3, must_not_discre = []):
         if (trans_col_name not in must_not_discre) or len(must_not_discre) == 0:
             tree_model = RandomForestClassifier(max_depth=Max_depth)
             tree_model.fit(df[trans_col_name].to_frame(), df[label_col_name].to_frame());
             new_col_name = trans_col_name + "_" + label_col_name + "_Proba";
             predict_proba = tree_model.predict_proba(df[trans_col_name].to_frame());
-            df[trans_col_name] = predict_proba[:,1];
-            df[trans_col_name] = df[trans_col_name].astype(str);
-            return tree_model, trans_col_name;
+            df[new_col_name] = predict_proba[:,1];
+            min_max = pd.concat(
+                [df.groupby([new_col_name])[trans_col_name].min(),
+                 df.groupby([new_col_name])[trans_col_name].max()], 
+                 axis=1);
+            self.support(df, new_col_name, min_max);
+            df[trans_col_name] = df[new_col_name];
+            df.drop(new_col_name, axis = 1, inplace = True);
+            return min_max, trans_col_name;
         return None, trans_col_name;
-    #===================================================================================================
+    #======================================================================================================
     def random_forest_all_cols(self, df, label_col_name, Max_depth = 3, must_not_discre = []):
         result = [];
         for i in df.columns:
             if df[i].dtypes != 'object' and i != label_col_name:
-                model = self.random_forest(df, i, label_col_name, Max_depth, must_not_discre);
-                result.append(model);
+                intervals, col = self.random_forest(df, i, label_col_name, Max_depth = Max_depth, must_not_discre = must_not_discre)
+                result.append((intervals, col));
         return result;
-    #===================================================================================================
-    def support_other_set(self, models, df, sample, must_not_discre = []):
-        for i in range(len(models)):
-            (model, col) = models[i];
-            if(model != None):
-                predict_proba = model.predict_proba(df[col].to_frame());
-                df[col] = predict_proba[:,1];
-                df[col] = df[col].astype(str);
+    #======================================================================================================
+    def support(self, df, col_name, min_max_intervals):
+        df['min_'+col_name] = np.nan;
+        df['max_'+col_name] = np.nan;
+        for i in range(0, df[col_name].shape[0]):
+            for j in range(0, min_max_intervals.shape[0]):
+                if df[col_name].values[i] <= min_max_intervals.values[j,1] and df[col_name].values[i] >= min_max_intervals.values[j,0]:
+                    df.set_value(i,'min_'+col_name, min_max_intervals.values[j,0]);
+                    df.set_value(i,'max_'+col_name, min_max_intervals.values[j,1]);
+                    break;
+        df['min_'+col_name] = df['min_'+col_name].astype(str);
+        df['max_'+col_name] = df['max_'+col_name].astype(str);
+        df[col_name] = df['min_'+col_name];
+        df = df.drop(['min_'+col_name, 'max_'+col_name],axis = 1, inplace=True);
         return;
+    #======================================================================================================
+    def support_all(self, df, list_intervals):
+        for i in list_intervals:
+            intervals, col = i;
+            print(type(intervals));
+            print('<class \'pandas.core.frame.DataFrame\'>');
+            if (type(intervals) != '<class \'NoneType\'>'):
+                self.support(df, col, intervals);
--- Preprocessing_Data/main/main.py
+++ Preprocessing_Data/main/main.py
@@ -14,7 +14,13 @@ if __name__=="__main__":
     telecom_cust.MonthlyCharges = all.pd.to_numeric(telecom_cust.MonthlyCharges, errors='coerce');
     conv.CONVERTING().label(telecom_cust, 'Churn');
     df = telecom_cust.iloc[:,1:];
-    m.MISSING().remove(df);
+    m.MISSING().mean(df);
+
+    #print(df.shape);
+    #discre.DISCRETISATION().binning_frequency(df, 'tenure', 3);
+    #discre.DISCRETISATION().binning_width(df, 'tenure', 3);
+    #discre.DISCRETISATION().k_mean(df, 'tenure', 4);
+    #print(df.tenure.unique());
 
     Y = df['Churn'].copy();
     X = df.drop('Churn', axis = 1);
@@ -27,34 +33,28 @@ if __name__=="__main__":
     print("X test  shape = (%d , %d)"%X_test.shape  + ", Y test  shape = (%d)"%Y_test.shape);
 
     X_train['Churn'] = Y_train;
-    models = discre.DISCRETISATION().random_forest_all_cols(X_train,'Churn', Max_depth = 2, must_not_discre = ['SeniorCitizen']);
-    X_val['Churn'] = Y_val;
-    diff_set_val = discre.DISCRETISATION().support_other_set(models, X_val, X_train);
-    X_test['Churn'] = Y_test;
-    diff_set_test = discre.DISCRETISATION().support_other_set(models, X_test, X_train);
+    list_intervals = discre.DISCRETISATION().random_forest_all_cols(X_train, 'Churn', Max_depth = 3, must_not_discre =['SeniorCitizen']);
+    
 
     X_train = X_train.drop("Churn", axis = 1);
-    X_val = X_val.drop('Churn', axis = 1);
-    X_test = X_test.drop('Churn', axis = 1);
-
-
+    #X_val = X_val.drop('Churn', axis = 1);
+    #X_test = X_test.drop('Churn', axis = 1);
+    discre.DISCRETISATION().support_all(X_val, list_intervals);
+    discre.DISCRETISATION().support_all(X_test, list_intervals);
+    
     X_train = all.pd.get_dummies(X_train);
     X_val = all.pd.get_dummies(X_val);
     X_test = all.pd.get_dummies(X_test);
-    list_missing_names_val  = np.setdiff1d(X_train.columns, X_val.columns);
-    list_missing_names_tra  = np.setdiff1d(X_val.columns, X_train.columns);
-    list_missing_names_tes = np.setdiff1d(X_train.columns, X_test.columns);
-
-    inte.INTEGRATION().add_cols(X_val, list_missing_names_val, 0);
-    inte.INTEGRATION().add_cols(X_test, list_missing_names_tes, 0);
-
-    inte.INTEGRATION().entity_idenfication_column_not_similar(X_train, X_train);
-
+    
+    print("X train shape = (%d , %d)"%X_train.shape + ", Y train shape = (%d)"%Y_train.shape);
+    print("X valid shape = (%d , %d)"%X_val.shape   + ", Y valid shape = (%d)"%Y_val.shape);
+    print("X test  shape = (%d , %d)"%X_test.shape  + ", Y test  shape = (%d)"%Y_test.shape);
 
     model = all.SVC(C = 50, kernel = 'rbf', gamma = 0.001);
     model.fit(X_val, Y_val);
     print(model.score(X_train, Y_train));
-
-
-
-
+    
+    
+    
+    
+    #
