a79e7662d1e1197d7a7aed6fdc692ed4e5ad978c
WindPham <48479585+WindPham@users.noreply.github.com>
2019-08-09 18:01:53 +0700

    Cập nhật file py và tìm hiểu lý thuyết + cài đặt NN
    Tìm hiểu mô hình neural network.

--- file_py/tensorflow_single_perceptron__gradientdescentoptimize.py
+++ file_py/tensorflow_single_perceptron__gradientdescentoptimize.py
@@ -1,4 +1,3 @@
-
 import tensorflow as tf
 import numpy as np
 import sklearn as skl
@@ -9,33 +8,32 @@ import matplotlib as mplt
 import matplotlib.pyplot as plt
 import time
 np.random.seed(int(time.time())%100);
+# this is target function
 def fun(x):
-    return 2*x[:,0] - 3*x[:,1] + 1;
+    return 2*x[:,0] - 3*x[:,1] + 1;#y = 2/3 x + 1/3
+# generate a dataset with N points.
 def generate_data(N):
     X_train = np.random.uniform(-10.0, 10.0 , size = (N, 2));
     Y_train = np.sign(fun(X_train));
-    rd_noise = np.random.choice(100,10000)
-    Y_train[rd_noise] = -1*Y_train[rd_noise];
-    plt.scatter(X_train[:,0], X_train[:,1], c = Y_train);
-    
-    plt.show()
     return X_train, Y_train;
-
-N = 10000
+# init  N = 100, training set is included X_train and Y_train
+N = 100
 X_train, Y_train = generate_data(N)
+# init W and b
+W = tf.Variable(tf.zeros([2,1])); #[0
+                                 #  0]
 
-W = tf.Variable(tf.zeros([2,1]));
-b = tf.Variable([-1], dtype=tf.float32);
+b = tf.Variable([-1], dtype=tf.float32);# bias
 
 x = tf.compat.v1.placeholder(tf.float32, [N, 2]);
 y = tf.compat.v1.placeholder(tf.float32);
 
 model = tf.matmul(x,W) + b;
 
-#cho nay khong biet cho loss = ?
-loss_value = tf.reduce_sum(tf.abs(tf.sign(model)-y)/2);
-
-gra_op = tf.compat.v1.train.GradientDescentOptimizer(0.01);
+# init loss_value? Right or Wrong?
+loss_value = tf.reduce_mean(tf.multiply(-y, model));
+# set learning rate = 1
+gra_op = tf.compat.v1.train.GradientDescentOptimizer(1);
 
 train = gra_op.minimize(loss_value);
 
@@ -44,9 +42,18 @@ init = tf.compat.v1.global_variables_initializer();
 sess = tf.compat.v1.Session();
 sess.run(init);
 
-for i in range(1000):
+for i in range(10000000):
     sess.run(train, {x: X_train, y: Y_train});
 
 cW, cb, c_loss = sess.run([W, b, loss_value], {x: X_train, y: Y_train});
 
 print("W: %s %s, b: %s, loss: %s"%(cW[0], cW[1], cb, c_loss));
+
+# y = 2/3 x + 1/3, [7, 5], [-8, -5]
+plt.scatter(X_train[:,0], X_train[:,1], c = Y_train);
+x = np.linspace(-15, 15, 1000000);
+y = 2.0/3.0 * x + 1.0/3.0
+plt.plot(x, y);
+y_new = list(cW[0])[0]/list(cW[1])[0] * x + float(cb)/list(cW[1])[0];
+plt.plot(x, y_new);
+plt.show();
